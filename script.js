262fba51d36943fe3cbbce83d8bd42d1523d4bda
weirdly0 <106332839+weirdly0@users.noreply.github.com>
2024-10-04 00:55:51 +0530

    script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const token = "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"
+const token = "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";
 
-const inputtxt=document.getElementById("input")
-const image=document.getElementById("image")
-const button=document.getElementById("btn")
+const inputtxt = document.getElementById("input");
+const image = document.getElementById("image");
+const button = document.getElementById("btn");
 
 async function query() {
-    image.src = "/loading.gif"
-	const response = await fetch(
-		"https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
-		{
-			headers: { Authorization: `Bearer ${token}` },
-			method: "POST",
-			body: JSON.stringify({"inputs": inputtxt.value}),
-		}
-	);  
-	const result = await response.blob();
-	return result;
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+        {
+            headers: { Authorization: `Bearer ${token}` },
+            method: "POST",
+            body: JSON.stringify({ "inputs": inputtxt.value }),
+        }
+    );
+    const result = await response.blob();
+    return result;
 }
 
-button.addEventListener('click',async function(){
+button.addEventListener('click', async function () {
+    image.src = "/loading.gif";  // Show the loading gif
     query().then((response) => {
-        const objecturl = URL.createObjectURL(response)
-        image.src = objecturl
+        const objecturl = URL.createObjectURL(response);
+        image.src = objecturl;   // Remove the loading gif and show the image
     });
-})
+});
